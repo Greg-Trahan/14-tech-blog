96e58624bebfda839ef153f2ce6ad58add35adbd
Greg Trahan <trahan.greg@gmail.com>
2023-02-12 21:22:36 -0600

    add logout functionality

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -25,16 +25,37 @@ router.get("/login", async (req, res) => {
 });
 
 router.get("/dashboard", async (req, res) => {
-  res.render("dashboard", { loggedIn: req.session.loggedIn });
+  try {
+    const blogData = await Blog.findAll({
+      // where: User.name === 0,
+      include: [{ model: User, attributes: ["name"] }],
+    });
+    console.log(User.name);
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    res.render("dashboard", { blogs, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/blog/:id", async (req, res) => {
-  const blogData = await Blog.findByPk(req.params.id);
-  res.render("blogs", { loggedIn: req.session.loggedIn, blog: blogData });
+  try {
+    const blogData = await Blog.findByPk(req.params.id);
+    if (!blogData) {
+      res.status(404).json({ message: "No blog with this id" });
+      return;
+    }
+    const blogs = blogData.get({ plain: true });
+    res.render("blogs", { loggedIn: req.session.loggedIn, blogs: blogs });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/logout", async (req, res) => {
-  res.render("logout");
+  res.render("logout", { loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
